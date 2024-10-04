2b6172307717133e831bb76d066768d638a816e6
BhavaniPenugonda <bhavani0202@gmail.com>
2024-10-04 15:15:29 +0530

    The state is initialized with list of movies

--- src/Components/main-view/main-view.jsx
+++ src/Components/main-view/main-view.jsx
@@ -2,13 +2,23 @@ import { useState } from "react";
 
 export const MainView = () => {
 
+  const [movies, setMovies] = useState([
+    { id: 1, title: "Inception" },
+    { id: 2, title: "The Matrix" },
+    { id: 3, title: "Interstellar" },
+    { id: 4, title: "The Dark Knight" },
+    { id: 5, title: "Pulp Fiction" }
+  ]);
+
+  if (movies.length === 0) {
+    return <div>The list is empty!</div>;
+  }
+
   return (
     <div>
-      <div>Eloquent JavaScript</div>
-      <div>Mastering JavaScript Functional Programming</div>
-      <div>JavaScript: The Good Parts</div>
-      <div>JavaScript: The Definitive Guide</div>
-      <div>The Road to React</div>
+      {movies.map((movie) => {
+        return <div key={movie.id}>{movie.title}</div>;
+      })}
     </div>
   );
 };
