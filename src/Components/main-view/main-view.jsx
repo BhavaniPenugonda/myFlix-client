67b8e5ca6f92e9583b4637b9431e08868eccb2b5
BhavaniPenugonda <bhavani0202@gmail.com>
2024-10-08 13:30:28 +0530

    movie.data is imported in main view

--- src/Components/main-view/main-view.jsx
+++ src/Components/main-view/main-view.jsx
@@ -1,59 +1,12 @@
 import { useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import {MovieView } from "../movie-view/movie-view";
+import { movies} from "../movie-data/movie-data";
 
 
-export const MainView = () => {
 
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "Inception",
-      description:
-        "A skilled thief, the absolute best in the dangerous art of extraction, steals valuable secrets from deep within the subconscious during the dream state.",
-      image: "https://upload.wikimedia.org/wikipedia/en/7/7f/Inception_ver3.jpg",
-      genre: "Sci-Fi, Thriller",
-      director: "Christopher Nolan"
-    },
-    {
-      id: 2,
-      title: "The Matrix",
-      description:
-        "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
-      image: "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",
 
-      genre: "Sci-Fi, Action",
-      director: "The Wachowskis"
-    },
-    {
-      id: 3,
-      title: "Interstellar",
-      description:
-        "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
-      image: "https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg",
-      genre: "Sci-Fi, Adventure",
-      director: "Christopher Nolan"
-    },
-    {
-      id: 4,
-      title: "The Dark Knight",
-      description:
-        "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
-      image: "https://upload.wikimedia.org/wikipedia/en/8/8a/Dark_Knight.jpg",
-      genre: "Action, Crime, Drama",
-      director: "Christopher Nolan"
-    },
-    {
-      id: 5,
-      title: "Pulp Fiction",
-      description:
-        "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
-      image: "https://upload.wikimedia.org/wikipedia/en/8/82/Pulp_Fiction_cover.jpg",
-      genre: "Crime, Drama",
-      director: "Quentin Tarantino"
-    }
-  ]);
-
+  export const MainView = () => {
   const [selectedMovie, setSelectedMovie] = useState(null);
 
   if (selectedMovie) {
@@ -80,4 +33,4 @@ export const MainView = () => {
       ))}
     </div>
   );
-}
+  };
