aa3b358ed642c587b69fe05bbc1735219e2a7d12
BhavaniPenugonda <bhavani0202@gmail.com>
2024-10-08 13:29:54 +0530

    movie object is created in seperate file

---
+++ src/Components/movie-data/movie-data.jsx
@@ -0,0 +1,50 @@
+export const movies = [
+
+  
+    {
+      id: 1,
+      title: "Inception",
+      description:
+        "A skilled thief, the absolute best in the dangerous art of extraction, steals valuable secrets from deep within the subconscious during the dream state.",
+      image: "https://upload.wikimedia.org/wikipedia/en/7/7f/Inception_ver3.jpg",
+      genre: "Sci-Fi, Thriller",
+      director: "Christopher Nolan"
+    },
+    {
+      id: 2,
+      title: "The Matrix",
+      description:
+        "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
+      image: "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",
+
+      genre: "Sci-Fi, Action",
+      director: "The Wachowskis"
+    },
+    {
+      id: 3,
+      title: "Interstellar",
+      description:
+        "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+      image: "https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg",
+      genre: "Sci-Fi, Adventure",
+      director: "Christopher Nolan"
+    },
+    {
+      id: 4,
+      title: "The Dark Knight",
+      description:
+        "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+      image: "https://upload.wikimedia.org/wikipedia/en/8/8a/Dark_Knight.jpg",
+      genre: "Action, Crime, Drama",
+      director: "Christopher Nolan"
+    },
+    {
+      id: 5,
+      title: "Pulp Fiction",
+      description:
+        "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+      image: "https://upload.wikimedia.org/wikipedia/en/8/82/Pulp_Fiction_cover.jpg",
+      genre: "Crime, Drama",
+      director: "Quentin Tarantino"
+    }
+  ];
