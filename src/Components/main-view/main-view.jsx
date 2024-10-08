db644f5db023faf1aeceb5fea186288dafd238e2
BhavaniPenugonda <bhavani0202@gmail.com>
2024-10-08 16:34:19 +0530

    useEffect hook is used

--- src/Components/main-view/main-view.jsx
+++ src/Components/main-view/main-view.jsx
@@ -1,12 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import {MovieView } from "../movie-view/movie-view";
-import { movies} from "../movie-data/movie-data";
+/*import { movies} from "../movie-data/movie-data"; */
 
+export const MainView = () => {
+  const [movies, setMovies] = useState([]);
 
+useEffect(() => {
+    fetch("https://flixmovies-1ddcfb2fa4c5.herokuapp.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.map((movie) => {
+          return {
+            _id: movie._id,
+            Title: movie.Title,
+            Description: movie.Description,
+            Genre: movie.Genre,
+            Director: movie.Director,
+            ImagePath: movie.ImagePath,
+            Featured: movie.Featured,
+          };
+        });
 
-
-  export const MainView = () => {
+        setMovies(moviesFromApi);
+      });
+  }, []);
+  
   const [selectedMovie, setSelectedMovie] = useState(null);
 
   if (selectedMovie) {
