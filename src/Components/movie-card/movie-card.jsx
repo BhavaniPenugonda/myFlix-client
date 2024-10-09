dccf59ddb6a82170a6f0670e413f2d52c6fa9563
BhavaniPenugonda <bhavani0202@gmail.com>
2024-10-09 23:19:21 +0530

    PropTypes are  added

--- src/Components/movie-card/movie-card.jsx
+++ src/Components/movie-card/movie-card.jsx
@@ -1,7 +1,7 @@
 // Here  import the PropTypes library
 import PropTypes from "prop-types";
 
-
+// The MovieCard function component
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
     <div
@@ -13,5 +13,18 @@ export const MovieCard = ({ movie, onMovieClick }) => {
     </div>
   );
 };
+
+// Here is where we define all the props constraints for the MovieCard
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string,
+    Description:PropTypes.string,
+    Genre:PropTypes.string,
+    Director:PropTypes.string
+
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired,
+};
+
 
 
