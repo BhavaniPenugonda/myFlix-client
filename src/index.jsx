2a6bec5be691b357be9f26cccaec3082400c6a2f
BhavaniPenugonda <bhavani0202@gmail.com>
2024-10-15 11:56:59 +0530

    import statement is deleted

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { MainView } from './Components/main-view/main-view';
 
-import "./movie-view.scss";
+
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
