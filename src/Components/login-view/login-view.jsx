12e684baffa178ad399527742cece1f5f20c620f
BhavaniPenugonda <bhavani0202@gmail.com>
2024-10-15 16:26:09 +0530

    bootstrap components button and form  are imported

--- src/Components/login-view/login-view.jsx
+++ src/Components/login-view/login-view.jsx
@@ -1,5 +1,8 @@
 
 import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
 export const LoginView = ({ onLoggedIn }) => {
 
   const [username, setUsername] = useState("");
