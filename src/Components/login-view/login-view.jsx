3ef41ddee8c29437609a5811369f77c851897da8
BhavaniPenugonda <bhavani0202@gmail.com>
2024-10-15 17:20:31 +0530

    username,password fields are created using bootstrap components

--- src/Components/login-view/login-view.jsx
+++ src/Components/login-view/login-view.jsx
@@ -41,21 +41,24 @@ export const LoginView = ({ onLoggedIn }) => {
   };
   
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username:
-        <input type="text"
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="formUsername">
+        <Form.Label>Username:</Form.Label>
+        <Form.Control
+      type="text"
         value={username}
         onChange={(e) => setUsername(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input type="password" 
+      </Form.Group>
+      <Form.Group controlId="formPassword">
+        <Form.Label>Password:</Form.Label>
+        <Form.Control
+      type="password" 
       value={password}
       onChange={(e) => setPassword(e.target.value)}  />
-      </label>
-      <button type="submit">Submit</button>
-    </form>
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   );
 };
-
