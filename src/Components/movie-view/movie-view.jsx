a403ec471b3c566838f5e27e2b5d9c87dbb67c87
BhavaniPenugonda <bhavani0202@gmail.com>
2024-10-18 13:39:55 +0530

    URL params is used and imported

--- src/Components/movie-view/movie-view.jsx
+++ src/Components/movie-view/movie-view.jsx
@@ -1,9 +1,8 @@
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import { Button, Card } from "react-bootstrap";
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 
 import "./movie-view.scss";
-
+/*
 export const MovieView = ({ movie,  onBackClick}) => {
   return (
     <Card className="h-100">
@@ -32,5 +31,36 @@ export const MovieView = ({ movie,  onBackClick}) => {
         </Col>
       </Row>
     </Card>
+  );
+};
+*/
+
+
+export const MovieView = ({ movies }) => {
+  const { movieId } = useParams();
+
+  const movie = movies.find((m) => m.id === movieId);
+
+  return (
+    <div>
+      <div>
+        <img className="w-100" src={movie.ImagePath} />
+      </div>
+      <div>
+        <span>Title: </span>
+        <span>{movie.Title}</span>
+      </div>
+      <div>
+        <span>Director: </span>
+        <span>{movie.Director}</span>
+      </div>
+      <div>
+        <span>Genre: </span>
+        <span>{movie.Genre}</span>
+      </div>
+      <Link to={'/'}>
+        <button className="back-button">Back</button>
+      </Link>
+    </div>
   );
 };
