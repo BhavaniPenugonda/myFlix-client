bf2e0e94d61e622c5dc073e3e094d0879a3622a4
BhavaniPenugonda <bhavani0202@gmail.com>
2024-10-18 12:52:38 +0530

    encodeURIComponent is called and Link is imported

--- src/Components/movie-card/movie-card.jsx
+++ src/Components/movie-card/movie-card.jsx
@@ -1,6 +1,8 @@
 // Here  import the PropTypes library
 import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 
 // The MovieCard function component
 export const MovieCard = ({ movie, onMovieClick }) => {
@@ -10,9 +12,9 @@ export const MovieCard = ({ movie, onMovieClick }) => {
       <Card.Body>
         <Card.Title>{movie.Title}</Card.Title>
         <Card.Text>{movie.Description}</Card.Text>
-        <Button onClick={() => onMovieClick(movie)} variant="link">
-          Open
-        </Button>
+        <Link to={'/movies/${encodeURIComponent(movie.id)}'}>
+          <Button variant="link">Open</Button>
+        </Link>
       </Card.Body>
     </Card>
   );
