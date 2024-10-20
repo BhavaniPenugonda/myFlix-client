7e2de9c6033d09f45f73f3160eae8e631e038f35
BhavaniPenugonda <bhavani0202@gmail.com>
2024-10-21 00:22:07 +0530

    authorization is added. Imported the profileview

--- src/Components/main-view/main-view.jsx
+++ src/Components/main-view/main-view.jsx
@@ -8,26 +8,64 @@ import Row from "react-bootstrap/Row";
 import Col from 'react-bootstrap/Col';
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { NavigationBar } from "../navigation-bar/navigation-bar";
+import { ProfileView } from "../profile-view/profile-view";
+
 
 
 export const MainView = () => {
-  
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
   const [movies, setMovies] = useState([]);
   const [user, setUser] = useState(null);
+  const [token, setToken] = useState(storedToken ? storedToken : null);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
   
 
 
   useEffect(() => {
-    
-    fetch("https://flixmovies-1ddcfb2fa4c5.herokuapp.com/movies")
+    if (!token) return;
+    fetch("https://flixmovies-1ddcfb2fa4c5.herokuapp.com/movies", {
+      headers: { Authorization: `Bearer ${token}` }, // Include the token in the request
+    })
       
       .then((response) => response.json())
       .then((data) => {
         
         setMovies(data);
         });
-  }, []);
+  }, [token]);
   
+  const toggleFavorite = async (movieId) => {
+    if (!user) return;
+
+    const isFavorite = user.FavoriteMovies.includes(movieId);
+    const method = isFavorite ? "DELETE" : "POST"; // DELETE to remove, POST to add
+
+    try {
+      const response = await fetch(
+        "https://flixmovies-1ddcfb2fa4c5.herokuapp.com/users/${user.Username}/movies/${movieId}",
+        {
+          method: method,
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to update favorite movies.");
+      }
+
+      const updatedUser = await response.json();
+      setUser(updatedUser);
+      localStorage.setItem("user", JSON.stringify(updatedUser));
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
  
 
 return (
@@ -96,14 +134,32 @@ return (
                   <Col>The list is empty!</Col>
                 ) : (
                   <>
-                    {movies.map((movie) => (
+                    {movies.map((movie) => {
+                      const isFavorite = user?.FavoriteMovies?.includes(
+                        movie._id
+                      ); // Check if the movie is a favorite
+                      return(
                       <Col className="mb-4" key={movie.id} md={3}>
-                        <MovieCard movie={movie} />
+                        <MovieCard movie={movie}
+                       onMovieClick={() => setSelectedMovie(movie)}
+                       onFavoriteToggle={() => toggleFavorite(movie._id)}
+                       isFavorite={isFavorite}  />
                       </Col>
-                    ))}
+                      );
+                    })}
                   </>
                 )}
               </>
+            }
+          />
+          <Route
+            path="/profile/:username"
+            element={
+              user ? (
+                <ProfileView token={token} setUser={setUser} movies={movies} />
+              ) : (
+                <Navigate to="/login" replace />
+              )
             }
           />
         </Routes>
