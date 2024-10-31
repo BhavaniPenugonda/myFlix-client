536dc0a67220e1b2fd092952582faa000f5c14fe
BhavaniPenugonda <bhavani0202@gmail.com>
2024-10-31 13:57:17 +0530

    imported useSelector and dispatch,props are removed

--- src/Components/navigation-bar/navigation-bar.jsx
+++ src/Components/navigation-bar/navigation-bar.jsx
@@ -1,8 +1,13 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { setUser } from "../../redux/reducers/user";
 
 
-export const NavigationBar = ({ user, onLoggedOut }) => {
+export const NavigationBar = () => {
+
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
   return (
     <Navbar bg="light" expand="lg">
       <Container>
@@ -30,7 +35,7 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
                 <Nav.Link as={Link} to={"/profile/${user.Username}"}>
                   Profile
                 </Nav.Link>
-                <Nav.Link onClick={onLoggedOut}>Logout</Nav.Link>
+                <Nav.Link onClick={() => dispatch(setUser(null))}>Logout</Nav.Link>
               </>
             )}
           </Nav>
