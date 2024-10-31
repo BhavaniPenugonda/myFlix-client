70db63320827d36c846fac8ed3baaaa010de5514
BhavaniPenugonda <bhavani0202@gmail.com>
2024-10-31 13:21:46 +0530

    movies is removed from array

--- src/Components/movie-view/movie-view.jsx
+++ src/Components/movie-view/movie-view.jsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router";
 import { Link } from "react-router-dom";
 
 import "./movie-view.scss";
-export const MovieView = ({ movies }) => {
+export const MovieView = () => {
   const { movieId } = useParams();
 
   const movie = movies.find((m) => m._id === movieId);
