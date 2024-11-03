181b87b4922811bf701437c93e1654c68b51daaf
BhavaniPenugonda <bhavani0202@gmail.com>
2024-11-03 19:50:36 +0530

    logout User is added

--- src/redux/reducers/user/user.js
+++ src/redux/reducers/user/user.js
@@ -3,12 +3,15 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const userSlice = createSlice({
 name: "user",
-initialState: {user:null},
+initialState: null,
 reducers: {
 setUser: (state, action) => {
 state.user = action.payload
-}
+},
+logoutUser:(state)=>{
+  return null;
+},
 }
 });
-export const { setUser } = userSlice.actions;
+export const { setUser ,logoutUser} = userSlice.actions;
 export default userSlice.reducer;
